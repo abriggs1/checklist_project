7c10493ca31aa260c535bd269fc5de5a616fbcf4
abriggs1 <abriggs1@tcstudents.tusculum.edu>
2017-04-27 09:31:27 -0400

    Create form.php

---
+++ form.php
@@ -0,0 +1,185 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Checklist for Bachelor of Arts in Information Technology</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><link href="checkstyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div id="mainForm">
+
+<div id="formHeader">
+	<h2 class="formInfo">Checklist for Bachelor of Arts in Information Technology</h2>
+</div>
+
+<br />
+
+<h3 class="formInfo">Please mark below which courses you have taken.</h3>
+	
+<form id="form1" name="form1" method="post" action="form.php"> <!-- Begin Form -->
+	<ul class="mainForm" id="majorcourses">
+		
+		<li><h3>Major Courses</h3></li>
+
+		<li class="mainForm"><label for="math140">MATH 140 Elementary Statistics:</label></li>
+		
+		<li class="mainForm">	
+			<input type="radio" name="math140" value="1" id="math140_1" />Yes
+			<input type="radio" name="math140" value="0" id="math140_0" />No
+		</li>
+
+		<li class="mainForm"><label for="busn201">BUSN 201 Principles of Economics I:</label></li>
+	
+		<li class="mainForm">
+			<input type="radio" name="busn201" value="1" id="busn201_1" />Yes
+			<input type="radio" name="busn201" value="0" id="busn201_0" />No
+		</li>
+
+		<li class="mainForm"><label for="busn210">BUSN 210 Principles of Management:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn210" value="1" id="busn210_1" />Yes
+			<input type="radio" name="busn210" value="0" id="busn210_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="busn211">BUSN 211 Accounting Principles I:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn211" value="1" id="busn211_1" />Yes
+			<input type="radio" name="busn211" value="0" id="busn211_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="busn305">BUSN 305 Principles of Marketing:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn305" value="1" id="busn305_1" />Yes
+			<input type="radio" name="busn305" value="0" id="busn305_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="busn322">BUSN 322 Principles of Finance:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn322" value="1" id="busn322_1" />Yes
+			<input type="radio" name="busn322" value="0" id="busn322_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="busn325">BUSN 325 Professional Communication:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn325" value="1" id="busn325_1" />Yes
+			<input type="radio" name="busn325" value="0" id="busn325_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="busn403">BUSN 403 Business Law and Ethics:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="busn403" value="1" id="busn403_1" />Yes
+			<input type="radio" name="busn403" value="0" id="busn403_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="math220">MATH 220 Discrete Math:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="math220" value="1" id="math220_1" />Yes
+			<input type="radio" name="math220" value="0" id="math220_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc105">CISC 105 Problem Solvoing and Algorithms:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc105" value="1" id="cisc105_1" />Yes
+			<input type="radio" name="cisc105" value="0" id="cisc105_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc200">CISC 200 Mgmt Info Systems and Sys Analysis:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc200" value="1" id="cisc200_1" />Yes
+			<input type="radio" name="cisc200" value="0" id="cisc200_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc215">CISC 215 Database Management:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc215" value="1" id="cisc215_1" />Yes
+			<input type="radio" name="cisc215" value="0" id="cisc215_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc310">CISC 310 Networking Fundamentals:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc310" value="1" id="cisc310_1" />Yes
+			<input type="radio" name="cisc310" value="0" id="cisc310_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc325">CISC 325 Information Assurance:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc325" value="1" id="cisc325_1" />Yes
+			<input type="radio" name="cisc325" value="0" id="cisc325_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc330">CISC 330 Software Engineering:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc330" value="1" id="cisc330_1" />Yes
+			<input type="radio" name="cisc330" value="0" id="cisc330_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc322">CISC 322 Electronic Commerce:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc322" value="1" id="cisc322_1" />Yes
+			<input type="radio" name="cisc322" value="0" id="cisc322_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc375">CISC 375 Web Design:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc375" value="1" id="cisc375_1" />Yes
+			<input type="radio" name="cisc375" value="0" id="cisc375_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc_erps">CISC xxx Enterprise Resource Planning Systems:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc_erps" value="1" id="cisc_erps_1" />Yes
+			<input type="radio" name="cisc_erps" value="0" id="cisc_erps_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc_sa">CISC xxx Systems Administration:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc_sa" value="1" id="cisc_sa_1" />Yes
+			<input type="radio" name="cisc_sa" value="0" id="cisc_sa_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc442">CISC 442 Information Technology:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc442" value="1" id="cisc442_1" />Yes
+			<input type="radio" name="cisc442" value="0" id="cisc442_0" />No
+		</li>
+		
+		<li class="mainForm"><label for="cisc352">BUSN 352 Business Service-Learning Practicum:</label></li>
+		
+		<li class="mainForm">
+			<input type="radio" name="cisc352" value="1" id="cisc352_1" />Yes
+			<input type="radio" name="cisc352" value="0" id="cisc352_0" />No
+		</li>
+		
+		<br />
+		
+		<?php include'processor.php'; ?>
+		
+		<li class="mainForm">
+			<input id="saveForm" class="mainForm" type="submit" value="Save Information" /> <!-- Submit Button -->
+		</li>
+	</ul>		
+</form> <!-- End Form -->
+
+</div>
+
+</body>
+</html>
